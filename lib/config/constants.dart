816b0aaa07292e0756658bb98488db857e078daf
hippieperm <leess0133@gmail.com>
2025-02-04 20:52:08 +0900

    상수추가

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 class AppConstants {
   static const double defaultPadding = 16.0;
   static const double defaultRadius = 8.0;
   static const defaultAnimationDuration = Duration(milliseconds: 300);
-} 
+  static const Color primaryColor = Color(0xff5d4899);
+  static const Color secondaryColor = Color(0xff3d3d3d);
+}
